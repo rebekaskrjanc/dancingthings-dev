f3f8688805cda4cd945c3271b0669c7c931b076a
Lovro Kalan <lovro.kalan1@gmail.com>
2018-12-09 13:58:08 +0100

    api routes

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,28 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var ctrlLokacije = require('../controllers/lokacije');
-var ctrlKomentarji = require('../controllers/komentarji');
 
-/* Lokacije */
-router.get('/lokacije', 
-  ctrlLokacije.lokacijeSeznamPoRazdalji);
-router.post('/lokacije', 
-  ctrlLokacije.lokacijeKreiraj);
-router.get('/lokacije/:idLokacije', 
-  ctrlLokacije.lokacijePreberiIzbrano);
-router.put('/lokacije/:idLokacije', 
-  ctrlLokacije.lokacijePosodobiIzbrano);
-router.delete('/lokacije/:idLokacije', 
-  ctrlLokacije.lokacijeIzbrisiIzbrano);
+var ctrlUsers = require('../controllers/users');
 
-/* Komentarji */
-router.post('/lokacije/:idLokacije/komentarji', 
-  ctrlKomentarji.komentarjiKreiraj);
-router.get('/lokacije/:idLokacije/komentarji/:idKomentarja', 
-  ctrlKomentarji.komentarjiPreberiIzbranega);
-router.put('/lokacije/:idLokacije/komentarji/:idKomentarja', 
-  ctrlKomentarji.komentarjiPosodobiIzbranega);
-router.delete('/lokacije/:idLokacije/komentarji/:idKomentarja', 
-  ctrlKomentarji.komentarjiIzbrisiIzbranega);
+
+// USERS routes.
+router.get('/users', // get all users
+  ctrlUsers.getUsers);
+
+router.get('/users/:userId', //get certain user
+  ctrlUsers.getUser);
+
+router.put('/users/:userId', 
+  ctrlUsers.updateUser); //updateWholeUser
+
+router.post('/users', 
+  ctrlUsers.createUser);
+
+router.delete('/users/:userId', 
+  ctrlUsers.deleteUser);
+
 
 module.exports = router;
