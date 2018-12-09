ab43c549e69af499ef0f05b2ddefa4b8f06de0ab
Lovro Kalan <lovro.kalan1@gmail.com>
2018-12-09 13:46:38 +0100

    user model created

---
+++ app_api/models/users.js
@@ -0,0 +1,21 @@
+'use strict';
+var mongoose = require('mongoose');
+var userEnums = require('./enums/users-enums');
+
+var commentSchema = new mongoose.Schema({
+  author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  commentText: {type: String, required: true},
+});
+
+var userSchema = new mongoose.Schema({
+  username: {type: String, required: true, unique: true, dropDups: true},
+  password: {type: String, required: true},
+  name: {type: String, required: true},
+  email: {type: String, unique: true, dropDups: true, required: true},
+  state: String,
+  gender: {type: String, required: true},
+  dance: {type: String, required: true},
+  comments: [commentSchema],
+});
+
+mongoose.model('User', userSchema, 'Users');
